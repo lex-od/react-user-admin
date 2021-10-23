6d5ac7f94c3bb25cdb02550af89bf171852f7ee7
Aleksey Yesin <esinru@gmail.com>
2021-10-23 15:37:29 +0300

    Adds MUI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import { Container } from "./common";
+import { Container } from './common';
 
 const App = () => {
-  return (
-    <Container>
-      <div>hello</div>
-    </Container>
-  );
+    return (
+        <Container>
+            <div>hello</div>
+        </Container>
+    );
 };
 
 export default App;
